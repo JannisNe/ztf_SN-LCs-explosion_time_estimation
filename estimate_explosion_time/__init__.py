937d8deb7948ff0904767131a06382548cf5830f
JannisNe <necker@physik.hu-berlin.de>
2019-12-05 14:30:31 +0100

    add register_ztf_bands

--- estimate_explosion_time/__init__.py
+++ estimate_explosion_time/__init__.py
@@ -1,1 +1,8 @@
 import estimate_explosion_time.shared
+import sncosmo
+from numpy import loadtxt
+
+for f in ['g', 'r', 'i']:
+    dat = loadtxt('/lustre/fs23/group/icecube/necker/ztf_bandpasses/ztf' + f + '.txt')
+    band = sncosmo.Bandpass(dat[:, 0], dat[:, 1], name='ztf' + f)
+    sncosmo.registry.register(band)
