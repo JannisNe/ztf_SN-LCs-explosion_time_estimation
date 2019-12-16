73cf3ce441624d39a432657b5016adc2197894c1
JannisNe <necker@physik.hu-berlin.de>
2019-12-16 13:27:04 +0100

    add plot lc function

--- estimate_explosion_time/core/analyse_fits_from_simulation/plots.py
+++ estimate_explosion_time/core/analyse_fits_from_simulation/plots.py
@@ -4,9 +4,13 @@ import os
 import logging
 import sncosmo
 import pickle
+import json
+import seaborn as sns
+from collections import OrderedDict
 from scipy.stats import kde
 from scipy.optimize import curve_fit
-from estimate_explosion_time.shared import plots_dir, get_custom_logger, main_logger_name
+from estimate_explosion_time.shared import plots_dir, get_custom_logger, main_logger_name, magnitude_system, \
+    flux_zp, pickle_dir, bandcolors
 
 
 logger = get_custom_logger(__name__)
@@ -15,6 +19,8 @@ logging.getLogger('matplotlib').setLevel('INFO')
 
 
 class Plotter:
+
+    sn_mag_sys = sncosmo.get_magsystem(magnitude_system)
 
     def __init__(self, dhandler, method):
         self.dhandler = dhandler
@@ -35,8 +41,14 @@ class Plotter:
         """
         logger.debug('plotting histogramm of delta t_exp')
 
+        # logger.debug(f'rhandler data = {self.rhandler.t_exp_dif[:10]} ...')
+        # logger.debug(f'indices = {self.dhandler.selected_indices}')
+
         tdif = np.array(self.rhandler.t_exp_dif)[self.dhandler.selected_indices]
         tdif_e = np.array(self.rhandler.t_exp_dif_error)[self.dhandler.selected_indices]
+
+        # logger.debug(f't_dif = {tdif[:10]} ...')
+        # logger.debug(f't_dif error = {tdif_e[:10]} ...')
 
         ic = [0, 0]
         ic[0], tmean, ic[1] = np.quantile(tdif, [0.5-cl/2, 0.5, 0.5+cl/2])
@@ -131,9 +143,15 @@ class Plotter:
 
         lc = data['lcs'][indice]
         id_orig = data['meta']['idx_orig'][indice]
+        bands = np.unique(lc['band'])
 
-        fit_results = self.rhandler.collected_data[indice]['fit_output']
-        mask = self.rhandler.collected_data[indice]["mask"]
+        collected_data = self.rhandler.collected_data[indice]
+
+        t_exp_true = collected_data['t_exp_true']
+        t_exp_fit = collected_data['t_exp_fit']
+        t_exp_fit_error = collected_data['t_exp_dif_error']
+        fit_results = collected_data['fit_output']
+        mask = collected_data["mask"]
 
         if fit_results[0]['ID'] != id_orig:
             raise PlotterError(f'original ID and ID of fitted event are different: \n '
@@ -148,11 +166,171 @@ class Plotter:
             model.set(z=fit_result['z'], t0=fit_result['t0'], amplitude=fit_result['amplitude'])
             models.append(model)
 
-        sncosmo.plot_lc(lc, fname=f'{self.lc_dir}/{indice}.pdf', model=models,
-                        model_label=fit_results['model'][mask])
+        fig, ax = plt.subplots()
+
+        plot_times = np.linspace(
+            min(lc['time'])-20,
+            max(lc['time'])+20,
+            round((max(lc['time']) - min(lc['time']) + 40))
+        )
+
+        for band in bands:
+
+            logger.debug(f'plotting band {band}')
+
+            band_mask = lc['band'] == band
+            band_flux = lc['flux'][band_mask]
+            band_color = bandcolors(band)
+
+            zp = Plotter.sn_mag_sys.zpbandflux(band)
+            magalt = [-2.5 * np.log10(f / zp) if f > 0 else flux_zp for f in band_flux]
+
+            logger.debug(f'\nflux: {band_flux} \nmags: {magalt} \nzeropoint: {zp}')
+            input('continue? ')
+
+            band_mag = [Plotter.sn_mag_sys.band_flux_to_mag(flux, band)
+                        if flux > 0
+                        else flux_zp
+                        for flux in band_flux]
+
+            ax.plot(lc['time'][band_mask], band_mag, 'o', label=band, color=band_color)
+
+            for model in models:
+
+                model_mag = model.bandmag(band, magnitude_system, plot_times)
+                ax.plot(plot_times, model_mag, color=band_color, alpha=1/len(models))
+
+        ax.axvline(t_exp_true, color='red', linestyle='--', label='true $t_{exp}$')
+        ax.axvline(t_exp_fit, color='red', label='fitted $t_{exp}$')
+        ax.fill_between(np.array([-t_exp_fit_error/2, t_exp_fit_error/2]) + t_exp_fit,
+                        y1=25, color='red', alpha=0.2)
+
+        ax.legend()
+        ax.set_xlabel('phase in MJD')
+        ax.set_ylabel(f'mag in {magnitude_system} system')
+
+        ax.set_ylim(top=25)
+
+        plt.gca().invert_yaxis()
+        plt.tight_layout()
+        plt.savefig(f'{self.lc_dir}/{indice}.pdf')
+        plt.close()
+
+        # sncosmo.plot_lc(lc, fname=f'{self.lc_dir}/{indice}.pdf', model=models,
+        #                 model_label=fit_results['model'][mask])
 
     def plot_lc_with_mosfit_fit(self, indice):
-        pass
+
+        json_name = f'{pickle_dir}/{self.dhandler.name}/mosfit/{indice}.json'
+
+        sns.reset_orig()
+        with open(json_name, 'r', encoding='utf-8') as f:
+            data = json.loads(f.read())
+            if 'name' not in data:
+                data = data[list(data.keys())[0]]
+        photo = data['photometry']
+        model = data['models'][0]
+        real_data = len([x for x in photo if 'band' in x and 'magnitude' in x and (
+                'realization' not in x or 'simulated' in x)]) > 0
+        band_attr = ['band', 'instrument', 'telescope', 'system', 'bandset']
+        band_list = list(set([tuple(x.get(y, '')
+                                    for y in band_attr) for x in photo
+                              if 'band' in x and 'magnitude' in x]))
+        real_band_list = list(set([tuple(x.get(y, '')
+                                         for y in band_attr) for x in photo
+                                   if 'band' in x and 'magnitude' in x and (
+                                           'realization' not in x or 'simulated' in x)]))
+        xray_instrument_attr = ['instrument', 'telescope']
+        xray_instrument_list = list(set([tuple(x.get(y, '')
+                                               for y in xray_instrument_attr) for x in photo
+                                         if 'instrument' in x and 'countrate' in x]))
+        real_band_list = list(set([tuple(x.get(y, '')
+                                         for y in band_attr) for x in photo
+                                   if 'band' in x and 'magnitude' in x and (
+                                           'realization' not in x or 'simulated' in x)]))
+        real_xray_instrument_list = list(set([tuple(x.get(y, '')
+                                                    for y in xray_instrument_attr) for x in photo
+                                              if 'instrument' in x and 'countrate' in x and (
+                                                      'realization' not in x or 'simulated' in x)]))
+
+        fig = plt.figure(figsize=(12, 8))
+        plt.gca().invert_yaxis()
+        # plt.gca().set_xlim(56600,56675)
+        # plt.gca().set_ylim(bottom=25, top=19)
+        plt.gca().set_xlabel('MJD')
+        plt.gca().set_ylabel('Apparent Magnitude')
+        used_bands = []
+        for full_band in band_list:
+            (band, inst, tele, syst, bset) = full_band
+
+            extra_nice = ', '.join(list(filter(None, OrderedDict.fromkeys((inst, syst, bset)).keys())))
+            nice_name = band + ((' [' + extra_nice + ']') if extra_nice else '')
+
+            realizations = [[] for x in range(len(model['realizations']))]
+            for ph in photo:
+                rn = ph.get('realization', None)
+                si = ph.get('simulated', False)
+                if rn and not si:
+                    if tuple(ph.get(y, '') for y in band_attr) == full_band:
+                        realizations[int(rn) - 1].append((
+                            float(ph['time']), float(ph['magnitude']), [
+                                float(ph.get('e_lower_magnitude', ph.get('e_magnitude', 0.0))),
+                                float(ph.get('e_upper_magnitude', ph.get('e_magnitude', 0.0)))],
+                            ph.get('upperlimit')))
+            numrz = np.sum([1 for x in realizations if len(x)])
+            for rz in realizations:
+                if not len(rz):
+                    continue
+                xs, ys, vs, us = zip(*rz)
+                label = '' if full_band in used_bands or full_band in real_band_list else nice_name
+                if max(vs) == 0.0:
+                    plt.plot(xs, ys, color=bandcolors(band),
+                             label=label, linewidth=0.5, alpha=0.1)
+                else:
+                    xs = np.array(xs)
+                    ymi = np.array(ys) - np.array([np.inf if u else v[0] for v, u in zip(vs, us)])
+                    yma = np.array(ys) + np.array([v[1] for v in vs])
+                    plt.fill_between(xs, ymi, yma, color=bandcolors(band), edgecolor=None,
+                                     label=label, alpha=1.0 / numrz, linewidth=0.0)
+                    plt.plot(xs, ys, color=bandcolors(band),
+                             label=label, alpha=0.1, linewidth=0.5)
+                if label:
+                    used_bands = list(set(used_bands + [full_band]))
+            if real_data:
+                for s in range(2):
+                    if s == 0:
+                        cond = False
+                        symb = 'o'
+                    else:
+                        cond = True
+                        symb = 'v'
+                    vec = [(float(x['time']), float(x['magnitude']),
+                            0.0 if 'upperlimit' in x else float(
+                                x.get('e_lower_magnitude', x.get('e_magnitude', 0.0))),
+                            float(x.get('e_upper_magnitude', x.get('e_magnitude', 0.0)))) for x in photo
+                           if 'magnitude' in x and ('realization' not in x or 'simulated' in x) and
+                           'host' not in x and 'includeshost' not in x and
+                           x.get('upperlimit', False) == cond and
+                           tuple(x.get(y, '') for y in band_attr) == full_band]
+                    if not len(vec):
+                        continue
+                    xs, ys, yls, yus = zip(*vec)
+                    label = nice_name if full_band not in used_bands else ''
+                    plt.errorbar(xs, ys, yerr=(yus, yls), color=bandcolors(band), fmt=symb,
+                                 label=label,
+                                 markeredgecolor='black', markeredgewidth=1, capsize=1,
+                                 elinewidth=1.5, capthick=2, zorder=10)
+                    plt.errorbar(xs, ys, yerr=(yus, yls), color='k', fmt=symb, capsize=2,
+                                 elinewidth=2.5, capthick=3, zorder=5)
+                    if label:
+                        used_bands = list(set(used_bands + [full_band]))
+
+        plt.margins(0.02, 0.1)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(f'{self.lc_dir}/{indice}.pdf')
+        plt.close()
+
 
     def get_dir(self, method):
         return f'{self.get_my_root_dir()}/{method}/{self.dhandler.selection_string}/'
