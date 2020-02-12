ec791ef716e7b7a338fecba1c88d7edacdd6c7cb
JannisNe <necker@physik.hu-berlin.de>
2020-02-12 11:30:38 +0100

    demo for explosion time estimation

---
+++ estimate_explosion_time/analyses/dummy/plot_lc.py
@@ -0,0 +1,22 @@
+from estimate_explosion_time.shared import get_custom_logger, main_logger_name, all_methods
+import logging
+
+logger = get_custom_logger(main_logger_name)
+logger.setLevel(logging.DEBUG)
+
+from estimate_explosion_time.core.analyse_fits_from_simulation.plots import Plotter
+from estimate_explosion_time.core.data_prep.data import DataHandler
+from estimate_explosion_time.shared import get_custom_logger, main_logger_name
+import logging
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ind', type=int)
+parser.add_argument('method', type=str)
+args = parser.parse_args()
+
+logger = get_custom_logger(main_logger_name)
+logger.setLevel(logging.DEBUG)
+dh = DataHandler.get_dhandler('simsurvey_simulation')
+plotter = Plotter(dh, args.method)
+plotter.plot_lc(args.ind)
