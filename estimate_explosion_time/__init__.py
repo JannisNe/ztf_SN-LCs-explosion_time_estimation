ff2ae701d8bf6b3c7551743f94042921cc20dd14
JannisNe <necker@physik.hu-berlin.de>
2020-02-19 15:59:18 +0100

    include ZTF_BANDPASS_DIR as environment variable

--- estimate_explosion_time/__init__.py
+++ estimate_explosion_time/__init__.py
@@ -1,8 +1,12 @@
 import estimate_explosion_time.shared
 import sncosmo
+import os
 from numpy import loadtxt
 
+
+ztf_bandpass_dir = os.environ['ZTF_BANDPASS_DIR']
+
 for f in ['g', 'r', 'i']:
-    dat = loadtxt('/lustre/fs23/group/icecube/necker/ztf_bandpasses/ztf' + f + '.txt')
+    dat = loadtxt(ztf_bandpass_dir + '/ztf' + f + '.txt')
     band = sncosmo.Bandpass(dat[:, 0], dat[:, 1], name='ztf' + f)
     sncosmo.registry.register(band)
